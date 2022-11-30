f2ed40ed0aee8d955185a1bf99f3350a53ee5755
Diego de Sousa Araujo <diesgo.araujo@gmail.com>
2022-11-30 15:21:49 -0300

    Modified the classes of errors

--- src/errors/errors.ts
+++ src/errors/errors.ts
@@ -1,16 +1,16 @@
-export class ExpirationDateErrors extends Error {
-    constructor(message: string) {
-        super(message);
-    }
-}
-
-export class LowInventoryError extends Error {
-    constructor(message: string) {
-        super(message);
-    }
-}
-
 export class InventoryError extends Error {
+    constructor(message: string) {
+        super(message);
+    }
+}
+
+export class ExpirationDateErrors extends InventoryError {
+    constructor(message: string) {
+        super(message);
+    }
+}
+
+export class LowInventoryError extends InventoryError {
     constructor(message: string) {
         super(message);
     }
