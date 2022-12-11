9d7b44fb2e2d8cc057e5e74d862db3deeb556c2a
Romero Antonio Ramos de Mendonça <romeromendonca22@gmail.com>
2022-12-11 19:54:44 -0300

    changed perishable model: changed constructor from ordered to named parameters, added serialization method (fromMap)

--- src/models/perishable.ts
+++ src/models/perishable.ts
@@ -1,10 +1,13 @@
-import { Item } from "./item";
+import Item from "./item";
+import { WareHouse } from "./warehouse";
+import WarehouseItem from "./warehouse_item";
 
-export class Perishable extends Item {
+export class Perishable extends WarehouseItem {
     private _expirationDate: Date;
     
-    constructor(id: number, description: string, insertionDate: Date, amount: number, location: string, expirationDate: Date) {
-        super(id, description, insertionDate, amount, location);
+    constructor({id, item, warehouse, insertionDate, amount, location, expirationDate} : 
+        {id: number; item: Item; warehouse: WareHouse, insertionDate: Date; amount: number; location: string; expirationDate: Date}) {
+        super({id, item, warehouse, insertionDate, amount, location})
         this._expirationDate = expirationDate;
     }
 
@@ -14,6 +17,19 @@ export class Perishable extends Item {
 
     set setExpirationDate(date: Date) {
         this._expirationDate = date;
+    }
+
+
+    static fromMap(map: Map<string, any>): WarehouseItem {
+        return new Perishable({
+            id: map.get('WARE_ITEM_ID'),
+            item: map.get('ITEM_ID'),
+            warehouse: map.get('WAREHOUSE_ID'),
+            insertionDate: new Date(<string> map.get('INSERTION_DATE')), 
+            amount: map.get('AMOUNT'), 
+            location: map.get('LOCATION'),
+            expirationDate: map.get('EXPIRATION_DATE')
+        });
     }
 
 }
