b77c64cc183b8f099f900c7e197dd5e1083711f2
Romero Antonio Ramos de Mendonça <romeromendonca22@gmail.com>
2022-12-13 19:23:54 -0300

    changed warehouse system: added method to find ware items by specific property; added method result feedbacks

--- system/warehouse_system.ts
+++ system/warehouse_system.ts
@@ -9,11 +9,11 @@ import { Perishable } from '../src/models/perishable';
 import utils from '../src/utils/utils';
 
 
-class WarehouseSystem {
+export default class WarehouseSystem {
 
     protected databaseRepository: DatabaseRepository;
     protected itemRepository: ItemRepository;
-    protected warehouseRepository: WarehouseRepository;
+    public warehouseRepository: WarehouseRepository;
     protected wareItemRepository: WareItemRepository;
 
     static async initialize(path: string) : Promise<WarehouseSystem> {
@@ -24,7 +24,7 @@ class WarehouseSystem {
             warehouseSystem.itemRepository = new ItemRepository(warehouseSystem.databaseRepository.db);
             warehouseSystem.warehouseRepository = new WarehouseRepository(warehouseSystem.databaseRepository.db);
             warehouseSystem.wareItemRepository = new WareItemRepository(warehouseSystem.databaseRepository.db);
-    
+
             return warehouseSystem;
         } catch (error) {
             console.log((<Error>error).message);
@@ -37,16 +37,24 @@ class WarehouseSystem {
             let item = new Item(0, description);
             await this.itemRepository.insertItem(item);
         } catch (error) {
-            
+            console.log((<Error>error).message);
         }
     }
 
     async findAllItems(): Promise<Item[]> {
         try {
             let items: Item[] = await this.itemRepository.findAllItems();
+
+            console.log (
+            "=================================================" + "\n" +
+            "=============== Itens Cadastrados ===============" + "\n" +
+            "=================================================" + "\n"
+            )
+
+            items.forEach((item) => console.log('> ' + item.toString()))
             return items;
         } catch (error) {
-            
+            console.log((<Error>error).message);
         }
     }
 
@@ -54,9 +62,10 @@ class WarehouseSystem {
         try {
             let id: number = utils.inputNumber('Id do item: ');
             let item: Item = await this.itemRepository.findItemById(id);
+            console.log('> ' + item.toString());
             return item;
         } catch (error) {
-            
+            console.log((<Error>error).message);
         }
     }
 
@@ -66,17 +75,19 @@ class WarehouseSystem {
             let newdescription = utils.inputText('Nova descrição: ', true);
             item.description = newdescription ?? item.description;
             await this.itemRepository.updateItem(item.id, item);
+            console.log('Operação Sucedida!');
         } catch (error) {
-            
+            console.log((<Error>error).message);
         }
     }
 
     async removeItem(): Promise<void> {
         try {
             let id: number = utils.inputNumber('Id do item: ');
-            await this.itemRepository.deleteItem(id);            
+            await this.itemRepository.deleteItem(id);
+            console.log('Operação Sucedida!');            
         } catch (error) {
-            
+            console.log((<Error>error).message);
         }
     }
 
@@ -85,65 +96,77 @@ class WarehouseSystem {
             let name: string = utils.inputText('Nome do item: ');
             let warehouse: WareHouse = new WareHouse(0, name);
             await this.warehouseRepository.insertWarehouse(warehouse);
+            console.log('Operação Sucedida!');
         } catch (error) {
-            
+            console.log((<Error>error).message);
         }
     }
 
-    async findAllWareHouses(): Promise<WareHouse[]> {
+    async findAllWarehouses(): Promise<WareHouse[]> {
         try {
             let warehouses: WareHouse[] = await this.warehouseRepository.findAllWarehouses();
+            console.log (
+            "===============================================" + "\n" +
+            "========== Almoxarifados Cadastrados ==========" + "\n" +
+            "===============================================" + "\n"
+            )
+            warehouses.forEach((warehouse) => console.log('> ' + warehouse.toString()));
             return warehouses;
         } catch (error) {
-            
+            console.log((<Error>error).message);
         }
     }
-
+    
     async findWarehouse(): Promise<WareHouse> {
         try {
             let id: number = utils.inputNumber('Id do almoxarifado: ');
             let warehouse: WareHouse = await this.warehouseRepository.findWarehouseById(id);
+            console.log('> ' + warehouse.toString())
             return warehouse;            
         } catch (error) {
-            
+            console.log((<Error>error).message);
         }
     }
-
+    
     async editWarehouse(): Promise<void> {
         try {
             let wareHouse: WareHouse = await this.findWarehouse();
             let newName: string = utils.inputText('Novo nome: ', true);
             wareHouse.name = newName ?? wareHouse.name;
             await this.warehouseRepository.updateWarehouse(wareHouse.id, wareHouse);
+            console.log('Operação Sucedida!');
         } catch (error) {
-            
+            console.log((<Error>error).message);
         }
     }
-
+    
     async removeWarehouse(): Promise<void> {
         try {
             let id: number = utils.inputNumber('Id do almoxarifado: ');
             await this.warehouseRepository.deleteWarehouse(id);
+            console.log('Operação Sucedida!');
             
         } catch (error) {
-            
+            console.log((<Error>error).message);
         }
     }
-
+    
     async addwareItem(): Promise<void> {
         try {
-            let type: string = utils.inputWareItemType('Tipo de item (N - Normal, P - Perecível)');
-            let item: Item = await this.findItem();
-            let warehouse: WareHouse = await this.findWarehouse();
+            let type: string = utils.inputWareItemType('Tipo de item (N - Normal, P - Perecível): ');
+            let itemId: number = utils.inputNumber('Id do Item: ');
+            let item: Item = await this.itemRepository.findItemById(itemId);
+            let warehouseId: number = utils.inputNumber('Id do Almoxarifado: ');
+            let warehouse = await this.warehouseRepository.findWarehouseById(warehouseId);
             let insertionDate: Date = new Date();
             let amount: number = utils.inputNumber('Quantidade de itens:');
-            let location: string = utils.inputLocation('Localizacao: ');
+            let location: string = utils.inputLocation('Localizacao (C_P_A_): ');
     
             let wareItem: WarehouseItem;
-    
-    
-            if(type == 'perishable') {
-                let expirationDate: Date = utils.inputDate('Data de vencimeto: ');
+            
+            
+            if(type == 'P') {
+                let expirationDate: Date = utils.inputDate('Data de vencimeto (AAAA-MM-DD): ');
     
                 wareItem = new Perishable({
                     id: 0, 
@@ -155,7 +178,7 @@ class WarehouseSystem {
                     expirationDate: expirationDate,
                 })
             } else {
-                let wareItem: WarehouseItem = new WarehouseItem({
+                wareItem = new WarehouseItem({
                     id: 0, 
                     item: item, 
                     warehouse: warehouse, 
@@ -164,60 +187,89 @@ class WarehouseSystem {
                     location: location,
                 });
             }
-    
             await this.wareItemRepository.insertWareItem(wareItem);
+            console.log('Operação Sucedida!');
         } catch (error) {
-            
+            console.log((<Error>error).message);
+        }
+    }
+    
+    async findAllWareItem(): Promise<WarehouseItem[]> {
+        try {
+            let wareItems: WarehouseItem[] = await this.wareItemRepository.findAllWareItems();
+
+            console.log (
+"=================================================" + "\n" +
+"=============== Lotes Cadastrados ===============" + "\n" +
+"=================================================" + "\n"
+ 
+            );
+            wareItems.forEach((wareItem) => console.log('> ' + wareItem.toString()));
+            return wareItems;            
+        } catch (error) {
+            console.log((<Error>error).message);
         }
     }
 
-    async findAllWareItem(): Promise<WarehouseItem[]> {
+    async findAllWareItemByProperty(): Promise<WarehouseItem[]> {
         try {
-            let wareItens: WarehouseItem[] = await this.wareItemRepository.findAllWareItems();
-            return wareItens;            
+            let property: string = utils.inputProperty();
+            let value: string = utils.inputAlphaNumeric('Valor desejado: ');
+            let wareItemsByProperty: WarehouseItem[] = await this.wareItemRepository.findAllWareItemsByProperty(property, value);
+            console.log (
+                "=================================================" + "\n" +
+                "======== Lotes Que Atendem a Propriedade ========" + "\n" +
+                "=================================================" + "\n"
+                
+                            );
+            wareItemsByProperty.forEach((wareItem) => console.log('> ' + wareItem.toString()));
+            return wareItemsByProperty;
         } catch (error) {
-            
+            console.log(error);
+            console.log((<Error>error).message);
         }
     }
 
     async findWareItem(): Promise<WarehouseItem> {
         try {
-            let property: string = utils.inputProperty();
-            let value: string = utils.inputAlphaNumeric('Valor desejado: ')
+            let property: string = 'WARE_ITEM_ID';
+            let value: string = utils.inputNumber('Id do item: ').toString();
     
             let wareItem: WarehouseItem = await this.wareItemRepository.findWareItemByProperty(property, value);
+            console.log('> ' + wareItem.toString());
             return wareItem;
         } catch (error) {
-            
+            console.log((<Error>error).message);
         }
     }
 
     async editWareItem(): Promise<void> {
         try {
-            let wareItem: WarehouseItem = await this.findWareItem();
-            wareItem.item = await this.findItem();
-            wareItem.warehouse = await this.findWarehouse();
+            let value: string = utils.inputNumber('Id do item: ').toString();
+            let wareItem: WarehouseItem = await this.wareItemRepository.findWareItemByProperty('WARE_ITEM_ID', value);
+            let itemId: number = utils.inputNumber('Id do Item: ');
+            wareItem.item = await this.itemRepository.findItemById(itemId);
+            let warehouseId: number = utils.inputNumber('Id do Almoxarifado: ');
+            wareItem.warehouse = await this.warehouseRepository.findWarehouseById(warehouseId);
             wareItem.amount = utils.inputNumber('Nova quantidade: ', true) ?? wareItem.amount;
-            wareItem.location = utils.inputLocation('Nova localização', true) ?? wareItem.location;
+            wareItem.location = utils.inputLocation('Nova localização: ', true) ?? wareItem.location;
     
             await this.wareItemRepository.updateWareItem(wareItem.id, wareItem);
+            console.log('Operação Sucedida!');
         } catch (error) {
-            
+            console.log((<Error>error).message);
         }
     }
 
     async deleteWareItem(): Promise<void> {
         try {
-            let wareItem: WarehouseItem = await this.findWareItem();            
-            await this.wareItemRepository.deleteWareItem(wareItem.id);
+            let id: number = utils.inputNumber('Id do Lote: ')             
+            await this.wareItemRepository.deleteWareItem(id);
+            console.log('Operação Sucedida!');
         } catch (error) {
-            
+            console.log((<Error>error).message);
         }
+        
     }
-
-
-
-
-
 
 }
