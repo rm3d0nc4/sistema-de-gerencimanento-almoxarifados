aaf9409169d32a900a3c9969dbdcac53bb57d44d
Romero Antonio Ramos de Mendonça <romeromendonca22@gmail.com>
2022-12-13 00:04:17 -0300

    added dependencies: prompt-sync
    Co-authored-by: Diego Araújo <Daliego@users.noreply.github.com>

--- src/errors/errors.ts
+++ src/errors/errors.ts
@@ -1,17 +1,13 @@
-export class InventoryError extends Error {
+export class AplicacaoError extends Error{
     constructor(message: string) {
         super(message);
     }
 }
 
-export class ExpirationDateErrors extends InventoryError {
+export class InputError extends AplicacaoError {
     constructor(message: string) {
         super(message);
     }
 }
 
-export class LowInventoryError extends InventoryError {
-    constructor(message: string) {
-        super(message);
-    }
-}
+
